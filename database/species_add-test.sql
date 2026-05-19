b8a8209ee03e7b98d598c2e05c9c4281b26586b0
SynapseWorks <126611424+SynapseWorks@users.noreply.github.com>
2026-05-19 12:18:13 -0400

    Create species_add-test.sql

---
+++ database/species_add-test.sql
@@ -0,0 +1,18 @@
+INSERT INTO species (
+    common_name,
+    scientific_name,
+    kingdom,
+    category,
+    description,
+    edible_status,
+    toxicity_status
+)
+VALUES (
+    'Garlic Mustard',
+    'Alliaria petiolata',
+    'Plantae',
+    'plant',
+    'An invasive edible plant with garlic-scented leaves.',
+    'edible with caution',
+    'generally low toxicity'
+);
